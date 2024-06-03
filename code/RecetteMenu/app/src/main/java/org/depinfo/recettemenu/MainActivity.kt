f98e74f76f17a8f690456b6d64ae195bd013fb82
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2024-06-03 12:39:47 -0400

    Ajout du menu

--- code/RecetteMenu/app/src/main/java/org/depinfo/recettemenu/MainActivity.kt
+++ code/RecetteMenu/app/src/main/java/org/depinfo/recettemenu/MainActivity.kt
@@ -1,20 +1,25 @@
 package org.depinfo.recettemenu
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.depinfo.recettemenu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
+
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        val inflater: MenuInflater = menuInflater
+//        inflater.inflate(R.menu.overflow_menu, menu)
+//        return true
+//    }
 }
