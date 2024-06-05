7531a7e883d3df2067120973117a5cd6aaa82e12
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2024-06-05 18:18:17 -0400

    wip

--- code/RecetteTiroir/app/src/main/java/org/depinfo/recettetiroir/MainActivity.kt
+++ code/RecetteTiroir/app/src/main/java/org/depinfo/recettetiroir/MainActivity.kt
@@ -1,7 +1,9 @@
 package org.depinfo.recettetiroir
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import org.depinfo.recettetiroir.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.accueil_item) {
+            Snackbar.make(binding.root, "On va à l'accueil!", Snackbar.LENGTH_SHORT).show()
+            return true
+        } else if(item.itemId == R.id.rechercher_item) {
+            Snackbar.make(binding.root, "On va rechercher!", Snackbar.LENGTH_SHORT).show()
+            return true
+        } else if (item.itemId == R.id.ajouter_item) {
+            Snackbar.make(binding.root, "On va ajouter!", Snackbar.LENGTH_SHORT).show()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- web/docs/01-cours/22-tiroir.md
+++ web/docs/01-cours/22-tiroir.md
@@ -5,3 +5,39 @@ hide_table_of_contents: true
 ---
 
 # Tiroir de navigation 🗄️
+
+<Row>
+
+<Column>
+
+:::danger Avant la séance (2h)
+
+<Row>
+
+<Column>
+
+</Column>
+
+<Column>
+
+</Column>
+
+</Row>
+
+:::
+
+</Column>
+
+<Column>
+
+:::info Pendant la séance
+
+:::
+
+</Column>
+
+</Row>
+
+:::note Exercices
+
+:::
