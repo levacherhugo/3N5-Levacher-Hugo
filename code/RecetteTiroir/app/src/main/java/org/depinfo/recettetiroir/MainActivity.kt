b21b1693368a7cb32bca107da533f9a52826e611
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2024-06-07 14:04:12 -0400

    Tiroir fonctionnel

--- code/RecetteTiroir/app/src/main/java/org/depinfo/recettetiroir/MainActivity.kt
+++ code/RecetteTiroir/app/src/main/java/org/depinfo/recettetiroir/MainActivity.kt
@@ -1,30 +1,88 @@
 package org.depinfo.recettetiroir
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import org.depinfo.recettetiroir.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupDrawer()
     }
 
+    private fun setupDrawer() {
+
+        // Afficher le menu hamburger sur la barre d'application
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        // Lier le tiroir de navigation à l'activité
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(this, binding.dlTiroir, R.string.ouvert, R.string.ferme)
+
+        // Faire en sorte que le menu hamburger se transforme en flèche au clic, et vis versa
+        binding.dlTiroir.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        // Réagir aux clics sur les actions de menu
+        binding.nvTiroir.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.accueil_item -> {
+                    Snackbar.make(binding.root, "On va à l'accueil!", Snackbar.LENGTH_SHORT).show()
+                }
+
+                R.id.ajouter_item -> {
+                    Snackbar.make(
+                        binding.root, "On va ajouter quelque chose!", Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+
+                R.id.rechercher_item -> {
+                    Snackbar.make(
+                        binding.root, "On va chercher quelquen chose!", Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            false
+        }
+    }
+
+    // Peut aussi se combiner avec le menu qui se retrouve dans la barre d'application
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == R.id.accueil_item) {
-            Snackbar.make(binding.root, "On va à l'accueil!", Snackbar.LENGTH_SHORT).show()
-            return true
-        } else if(item.itemId == R.id.rechercher_item) {
-            Snackbar.make(binding.root, "On va rechercher!", Snackbar.LENGTH_SHORT).show()
-            return true
-        } else if (item.itemId == R.id.ajouter_item) {
-            Snackbar.make(binding.root, "On va ajouter!", Snackbar.LENGTH_SHORT).show()
-            return true
+        // Réagir au clic sur le menu hamburger
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    // Les deux méthodes suivantes permettent de synchroniser le menu hamburger
+    // après avoir effectué une rotation de l'écran
+    // Pour mieux comprendre :
+    // 1. Commentez ces deux méthodes
+    // 2. Relancer l'application
+    // 3. Ouvrez le menu hamburger
+    // 4. Tournez le péripérique pour qu'il soit en mode paysage
+    // 5. Notez ce qui est arrivé au menu hambuger
+    // 6. Recommencez, mais en décommentant les deux méthodes
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        actionBarDrawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        actionBarDrawerToggle.onConfigurationChanged(newConfig)
+        super.onConfigurationChanged(newConfig)
     }
 }
