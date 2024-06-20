8e2f6961a360a6f05251f0bcff84aa4affe69050
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2024-06-20 16:22:42 -0400

    WIP

--- code/VideoOutilsDeboggage/app/src/main/java/org/depinfo/videooutilsdeboggage/MainActivity.kt
+++ code/VideoOutilsDeboggage/app/src/main/java/org/depinfo/videooutilsdeboggage/MainActivity.kt
@@ -1,6 +1,7 @@
 package org.depinfo.videooutilsdeboggage
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import org.depinfo.videooutilsdeboggage.databinding.ActivityMainBinding
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity() {
 
         setupBtn1()
         setupBtn2()
+
+        bouclePasSiComplique()
     }
 
     private fun setupBtn1() {
@@ -27,6 +30,15 @@ class MainActivity : AppCompatActivity() {
     private fun setupBtn2() {
         binding.btn2.setOnClickListener {
             Snackbar.make(binding.root, "Bouton 2 appuyé", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun bouclePasSiComplique() {
+        for (i in 1..1000) {
+            val x = i
+            val y = 1000 / i
+            Log.i("MON_SUPER_LOG", "x = $x, y = $y")
+            Log.e("UN AUTRE LOG", "a = $x, b = $y")
         }
     }
 }
