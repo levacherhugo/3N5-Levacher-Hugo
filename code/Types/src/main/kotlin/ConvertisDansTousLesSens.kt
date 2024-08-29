0cdc8206bb91ebc4b6c33fc6eacabf5751a1cf95
levacherhugo <2362405@cegepmontpetit.ca>
2024-08-29 12:25:40 -0400

    FCT: Exercice ConvertisDansTousLesSens

---
+++ code/Types/src/main/kotlin/ConvertisDansTousLesSens.kt
@@ -0,0 +1,14 @@
+package org.levacher
+
+fun main(){
+    val A:Int=5
+    println("int to float: "+A.toFloat())
+    val B:Double=5.0
+    println("double to int: "+B.toInt())
+    val C:String="5"
+    println("string to int: "+C.toInt())
+    val D:Int=5
+    println("int to string: "+D.toString())
+    val E:Float=5.0f
+    println("float to double: "+E.toDouble())
+}
