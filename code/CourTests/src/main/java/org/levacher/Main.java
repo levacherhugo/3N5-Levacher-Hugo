8c4c836eea1a55eb24757a81ebb90580d53934a1
Hugo Levacher <2362405@cegepmontpetit.ca>
2024-11-26 09:29:01 -0500

    FCT: exercices cours test

---
+++ code/CourTests/src/main/java/org/levacher/Main.java
@@ -0,0 +1,11 @@
+package org.levacher;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+fun isValidZipCode(zippy: String): Boolean {
+    return true
+}
